70bcb6630d067e4483bac0987cb5d7bdb5873996
Usuario <Usuario@DAVID>
2022-02-21 16:18:49 +0100

    He cambiado el nombre del botón que abre la ventana de calendario

--- EjemploForms/VentanaPrincipal.Designer.cs
+++ EjemploForms/VentanaPrincipal.Designer.cs
@@ -29,19 +29,19 @@ namespace EjemploForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.boton_calendario = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // boton_calendario
             // 
-            this.button1.Location = new System.Drawing.Point(78, 58);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 121);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "CALENDARIO";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.boton_calendario.Location = new System.Drawing.Point(78, 58);
+            this.boton_calendario.Name = "boton_calendario";
+            this.boton_calendario.Size = new System.Drawing.Size(126, 121);
+            this.boton_calendario.TabIndex = 0;
+            this.boton_calendario.Text = "CALENDARIO";
+            this.boton_calendario.UseVisualStyleBackColor = true;
+            this.boton_calendario.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -59,7 +59,7 @@ namespace EjemploForms
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.boton_calendario);
             this.Name = "VentanaPrincipal";
             this.Text = "VentanaPrincipal";
             this.ResumeLayout(false);
@@ -68,7 +68,7 @@ namespace EjemploForms
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button boton_calendario;
         private System.Windows.Forms.Button button2;
     }
 }
